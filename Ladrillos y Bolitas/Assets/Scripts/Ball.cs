dcb11917274d70094f54e8578fb41d2bb547bf07
Raúl Serrano Gómez <raulserranodev@gmail.com>
2018-11-23 15:44:12 +0100

    Pelota que rebota. Primer laboratorio

---
+++ Ladrillos y Bolitas/Assets/Scripts/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Use this for initialization
+    void Start () {
+        rb.velocity = new Vector2(5.0f, 5.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
