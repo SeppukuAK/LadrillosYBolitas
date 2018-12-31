9ffff5367c15823a934342b264f6c57515cdd674
Adriana <Adridelcastillodev@gmail.com>
2018-12-31 18:05:27 +0100

    Empezando remake del menú :)

---
+++ Ladrillos y Bolitas/Assets/Scripts/UIMenu/CellUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CellUI : MonoBehaviour {
+
+    private int _level;
+    
+    [SerializeField] private Text textLevel;
+    [SerializeField] private Image[] starsImage;
+
+    public void Init (int level, int stars) {
+        _level = level;
+        textLevel.text = (level+1).ToString();
+
+        //Inicialización de estrellas
+        for (int i = 0; i < stars; i++)
+        {
+            starsImage[i].enabled = true;
+        }
+	}
+	
+    public void OnClick()
+    {
+        GameManager.Instance.MapLevel = _level;
+        MenuCanvas.Instance.PlayLevelOptions("GameScene", true);
+    }
+}
